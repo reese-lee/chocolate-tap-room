b5e59d467f2e3c827b95f8094ca243d8076bf6c7
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 14:34:08 -0700

    add a horizontal line with styling to show on all pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Error404 from './Error404';
 import DrinksList from './DrinksList';
 import Splash from './Splash';
 import NavBar from './NavBar';
+import AddDrink from './AddDrink';
+import Employees from './Employees';
+import About from './About';
+
+const useStyles = makeStyles ({
+  hr: {
+    margin: '20%'
+  }
+})
 
 function App() {
+  const classes = useStyles();
   return(
     <div>
       <NavBar/>
       <Switch>
         <Route exact path='/' component={Splash} />
         <Route exact path='/drinks' component={DrinksList} />
+        <Route exact path='/addDrink' component={AddDrink} />
+        <Route exact path='/employees' component={Employees} />
+        <Route exact path='/about' component={About} />
         <Route component={Error404}/>
       </Switch>
+      <div className={classes.hr}>
+        <hr></hr>
+      </div>
     </div>
   );
 }
