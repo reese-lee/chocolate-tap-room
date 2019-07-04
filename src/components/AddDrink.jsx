866bcf091e6e19f09046e0c39db84f646c82edd0
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 15:18:54 -0700

    import form styling from material, looks so good!

--- src/components/AddDrink.jsx
+++ src/components/AddDrink.jsx
@@ -1,42 +1,87 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-const useStyles = makeStyles ({
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    flexBasis: 200,
+  },
   title: {
-    color: 'purple',
-    fontSize: '25pt'
+    fontSize: '4em',
+    textAlign: 'center'
   }
-});
+}));
 
-export default function AddDrink() {
+export default function FormTest() {
   const classes = useStyles();
-  return(
+  const [values, setValues] = React.useState({
+    name: '',
+    brand: '',
+    price: '',
+    description: ''
+  });
+
+  return (
     <div>
-      <div className={classes.title}>
-        Add a new drink here!
+    <div style={{fontFamily: 'DM Serif Display'}} className={classes.title}>
+      Add a new drink here!
+    </div>
+    <br></br>
+    <div className={classes.root}>
+      <TextField
+        id="outlined-simple-start-adornment"
+        className={clsx(classes.margin, classes.textField)}
+        variant="outlined"
+        label="Name of Drink"
+        InputProps={{
+          startAdornment: <InputAdornment position="start"></InputAdornment>,
+        }}
+      />
+
+      <TextField
+        id="outlined-simple-start-adornment"
+        className={clsx(classes.margin, classes.textField)}
+        variant="outlined"
+        label="Brand"
+        InputProps={{
+          startAdornment: <InputAdornment position="start"></InputAdornment>,
+        }}
+      />
+
+      <TextField
+        id="outlined-simple-start-adornment"
+        className={clsx(classes.margin, classes.textField)}
+        variant="outlined"
+        label="Price"
+        InputProps={{
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        }}
+      />
+
+      <TextField
+        id="outlined-simple-start-adornment"
+        className={clsx(classes.margin, classes.textField)}
+        variant="outlined"
+        label="Description"
+        InputProps={{
+          startAdornment: <InputAdornment position="start"></InputAdornment>,
+        }}
+      />
       </div>
-      <form>
-        <input
-          type='text'
-          id='name'
-          placeholder='Name of Drink'/>
-          <br></br>
-        <input
-          type='text'
-          id='brand'
-          placeholder='Brand'/>
-          <br></br>
-        <input
-          type='text'
-          id='price'
-          placeholder='Price'/>
-          <br></br>
-        <textarea
-          id='description'
-          placeholder='Description'/>
-        <br></br>
-        <button type='submit'>Add drink!</button>
-      </form>
     </div>
   );
 }
