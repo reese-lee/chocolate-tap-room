067d66bc00098f6754dec558da01709d04ae3214
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 12:25:43 -0700

    add styling to AddDrink form

--- src/components/AddDrink.jsx
+++ src/components/AddDrink.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles ({
+  title: {
+    color: 'purple',
+    fontSize: '25pt'
+  }
+});
 
 export default function AddDrink() {
+  const classes = useStyles();
   return(
     <div>
+      <div className={classes.title}>
+        Add a new drink here!
+      </div>
       <form>
         <input
           type='text'
           id='name'
           placeholder='Name of Drink'/>
+          <br></br>
         <input
           type='text'
           id='brand'
           placeholder='Brand'/>
+          <br></br>
         <input
           type='text'
-          id='Price'
+          id='price'
           placeholder='Price'/>
+          <br></br>
         <textarea
           id='description'
           placeholder='Description'/>
-
+        <br></br>
         <button type='submit'>Add drink!</button>
       </form>
     </div>
