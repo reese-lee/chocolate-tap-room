f51b14a363c08e2cad016687cee8f9be3319e070
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 10:18:01 -0700

    add props for drinks

---
+++ src/components/Drinks.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Drinks(props){
+  return (
+    <div>
+      <h4>{props.name}</h4>
+      <h5><{props.brand}/h5>
+      <h5>{props.price}</h5>
+      <h5>{props.flavor}</h5>
+      <h5>{props.description}</h5>
+    </div>
+  )
+}
+
+export default Drinks;
