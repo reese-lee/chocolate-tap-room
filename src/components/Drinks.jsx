91064573d906a88deb43d635f163f137a0c2b986
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 16:35:30 -0700

    add prop for pints

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -4,10 +4,10 @@ function Drinks(props){
   return (
     <div>
       <h4>{props.name}</h4>
-      <h5>{props.brand}</h5>
       <h5>{props.price}</h5>
       <h5>{props.flavor}</h5>
       <h5>{props.description}</h5>
+      <h5>{props.pints}</h5>
     </div>
   )
 }
