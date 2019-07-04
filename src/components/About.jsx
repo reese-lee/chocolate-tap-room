e81a742233ccc990635ce3c5120559d149818195
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 16:35:08 -0700

    change header font color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CardMedia from '@material-ui/core/CardMedia';
-import splash from './../assets/images/splash.jpg';
+import Splash from './Splash';
+import owner from './../assets/images/owner.jpg'
 
 const useStyles = makeStyles({
   image: {
-    width: '100%',
-    position: 'absolute',
-    margin: '0'
+    width: '70%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
   },
-  centered: {
-    display: 'flex',
-    justifyContent: 'left',
-    width: '60%',
+  title: {
+    fontSize: '4em',
+    textAlign: 'center',
+    color: '#36d9a0'
   },
-  fonts:{
-    color: 'white',
-    fontSize: '5em',
-    opacity: 0.85
+  bio: {
+    fontSize: '1em',
+    textAlign: 'center'
   }
-
 });
 
 const backgroundImage = {
-  backgroundImage: `url(${splash})`
+  backgroundImage: `url(${owner})`
 };
 
 export default function About() {
   const classes = useStyles();
   return (
     <div>
-    <Splash/>
-
-      Test
+      <div className={classes.title} style={{fontFamily: 'DM Serif Display'}}>
+        About Ella
+      </div>
+      <img className={classes.image} src={owner}/>
+      <br></br>
+      <div className={classes.bio} style={{fontFamily: 'Montserrat'}}>
+        Here will go some text about Ella.
+      </div>
     </div>
-
 
   );
 }
