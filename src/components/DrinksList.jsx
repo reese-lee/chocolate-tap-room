6829cd8758f854a27fe0876832c3ec033c8a6a0a
Reese Lee <reese.lee.cy@gmail.com>
2019-07-04 10:17:47 -0700

    add hard-coded drink objects

---
+++ src/components/DrinksList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Drinks from './Drinks';
+
+const availableDrinks = [
+  {
+    name: 'The Lucy Liu',
+    brand: 'House',
+    price: '$6.99',
+    flavor: 'Spicy Milk Chocolate',
+    description: 'Our popular, old-fashioned milk chocolate with a kick of jalapeno and chili spices.'
+  },
+  {
+    name: 'The Old-Fashioned',
+    brand: 'House',
+    price: '$4.99',
+    flavor: 'Milk Chocolate',
+    description: "The original recipe that started in Grandma's kitchen, and built us into a nationwide chocolate force."
+  },
+  {
+    name: 'The Herb Garden',
+    brand: 'House',
+    price: '$6.99',
+    flavor: 'Earthy and herby',
+    description: 'Made with thyme, rosemary, and oregano, this one is straight from the backyard!'
+  },
+
+]
