8a222109a1fe1ee7c9bf38d92c4eec141060e754
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 15:46:30 -0700

    add component to handle confirmation question for employee user

---
+++ src/components/EmployeeConfirmation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function EmployeeConfirmation(props){
+  return (
+    <div>
+      <p>Are you really an employee of this cafe?</p>
+      <button onClick={props.onEmployeeConfirmation}>Yes</button>
+    </div>
+  );
+}
+
+EmployeeConfirmation.propTypes = {
+  onEmployeeConfirmation: PropTypes.func
+};
+
+export default EmployeeConfirmation;
