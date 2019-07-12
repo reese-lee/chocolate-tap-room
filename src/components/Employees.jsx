4c3cbb9195b3c477c11a4c14e2a2faf0c08d59e1
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 11:29:11 -0700

    add render() to Employees while still trying to figure out the best component structure

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -32,7 +32,8 @@ class Employees extends React.Component {
       name: null,
       brand: null,
       price: null,
-      description: null
+      description: null,
+      pints: 124
     }
     this.handleAddNewDrink = this.handleAddNewDrink.bind(this);
   }
@@ -50,17 +51,20 @@ class Employees extends React.Component {
     })
   }
   const classes = useStyles();
-{/*  return (
-    <div>
-      <div className={classes.title} style={{fontFamily: 'DM Serif Display'}}>
-        What would you like to accomplish today?
+  render() {
+    return(
+      <div>
+        <div className={classes.title} style={{fontFamily: 'DM Serif Display'}}>
+          What would you like to accomplish today?
+        </div>
+        <div className={classes.text} style={{fontFamily: 'DM Serif Display'}}>
+          <Link style={{ textDecoration: 'none' }} to="/addDrink"><a className={classes.clicky}>Add a new drink</a></Link>
+          <br></br>
+          <Link style={{ textDecoration: 'none' }} to="/edit"><a className={classes.clicky}>Edit drinks</a></Link>
+        </div>
       </div>
-      <div className={classes.text} style={{fontFamily: 'DM Serif Display'}}>
-        <Link style={{ textDecoration: 'none' }} to="/addDrink"><a className={classes.clicky}>Add a new drink</a></Link>
-        <br></br>
-        <Link style={{ textDecoration: 'none' }} to="/edit"><a className={classes.clicky}>Edit drinks</a></Link>
-      </div>
-    </div> */}
-)}
+
+    )
+  }
 
 export Employees;
