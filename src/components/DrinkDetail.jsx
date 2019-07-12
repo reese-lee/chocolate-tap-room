8b7fc8d4defbb8aa5c4428bf008f36b4a2d9103d
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 15:46:49 -0700

    add component to handle details of drinks

---
+++ src/components/DrinkDetail.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function DrinkDetail(props){
+  return (
+    <div>
+      <hr/>
+      <h2>{props.selectedDrink.name}</h2>
+      <h3>{props.selectedDrink.brand}</h3>
+      <h3>{props.selectedDrink.price}</h3>
+      <h3>{props.selectedDrink.description}</h3>
+      <hr/>
+    </div>
+  );
+}
+
+DrinkDetail.propTypes = {
+  selectedDrink: PropTypes.object
+};
+
+export default DrinkDetail;
