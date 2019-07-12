67f33d917df0ac94b6dd3f333f777334b4225e69
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 14:11:51 -0700

    edit onNewDrink prop

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import AddDrink from './AddDrink';
 
 const useStyles = makeStyles({
   title: {
@@ -24,35 +23,22 @@ const useStyles = makeStyles({
   },
 })
 
-
 class Employees extends React.Component {
-
   constructor(){
     super();
     this.state = {
-      name: null,
-      brand: null,
-      price: null,
-      description: null,
-      pints: 124
+      masterDrinklist: {}
     }
-    this.handleAddNewDrink = this.handleAddNewDrink.bind(this);
+    this.handleAddingNewDrinkToList = this.handleAddingNewDrinkToList.bind(this);
   }
 
-  handleAddNewDrink(drink) {
-    let name = drink.name
-    let brand = drink.brand
-    let price = drink.price
-    let description = drink.description
-    this.setState({
-      name: name,
-      brand: brand,
-      price: price,
-      description: description
-    })
+  handleAddingNewDrinkToList(newDrink) {
+    let newDrinkId = v4()
+    let newMasterDrinkList = Object.assign({}, this.state.masterDrinkList, {
+      [newDrinkId]: newDrink
+    });
+    this.setState({masterDrinkList: newMasterDrinkList})
   }
-
-
 
   render() {
     return(
@@ -61,7 +47,7 @@ class Employees extends React.Component {
           What would you like to accomplish today?
         </div>
         <div style={{fontFamily: 'DM Serif Display'}}>
-          <Link style={{ textDecoration: 'none' }} to="/addDrink"><a>Add a new drink</a></Link>
+          <Link style={{ textDecoration: 'none' }} to="/addDrink" AddDrink onNewDrink={this.handleAddingNewDrinkToList}><a>Add a new drink</a></Link>
           <br></br>
           <Link style={{ textDecoration: 'none' }} to="/edit"><a>Edit drinks</a></Link>
         </div>
