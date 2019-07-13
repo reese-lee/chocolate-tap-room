ed732fb0095047392ea7b508f4c1ec0235914870
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 17:21:23 -0700

    edit routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import Error404 from './Error404';
 import DrinksList from './DrinksList';
 import Splash from './Splash';
 import NavBar from './NavBar';
 import AddDrink from './AddDrink';
-import Employees from './Employees';
+import EmployeeConfirmation from './EmployeeConfirmation';
 import About from './About';
-import { v4 } from 'uuid';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      masterDrinklist: {},
-      selectedDrink: null
-    }
+      masterDrinklist: [
+        {
+         name: 'The Lucy Liu',
+         price: '$6.99',
+         flavor: 'Spicy Milk Chocolate',
+         description: 'Our popular, old-fashioned milk chocolate with a kick of jalapeno and chili spices.'
+       },
+       {
+         name: 'The Old-Fashioned',
+         price: '$4.99',
+         flavor: 'Milk Chocolate',
+         description: "The original recipe that started in Grandma's kitchen, and built us into a nationwide chocolate force."
+       },
+       {
+         name: 'The Herb Garden',
+         price: '$6.99',
+         flavor: 'Earthy and herby',
+         description: 'Made with thyme, rosemary, and oregano, this one is straight from the backyard!'
+       }
+      ]
+    };
     this.handleAddingNewDrinkToList = this.handleAddingNewDrinkToList.bind(this);
-    this.handleEditingSelectedDrink = this.handleEditingSelectedDrink.bind(this);
   }
 
   handleAddingNewDrinkToList(newDrink) {
-    let newDrinkId = v4()
-    let newMasterDrinkList = Object.assign({}, this.state.masterDrinkList, {
-      [newDrinkId]: newDrink
-    });
-    this.setState({masterDrinkList: newMasterDrinkList})
-  }
-
-  handleEditingSelectedDrink(drinkId){
-    this.setState({selectedDrink: drinkId});
+    let newMasterDrinkList = this.state.masterDrinkList.push(newDrink);
+    this.setState({masterDrinkList: newMasterDrinkList});
   }
 
   render() {
@@ -38,23 +46,15 @@ class App extends React.Component {
       <div>
         <NavBar/>
         <Switch>
-          <Route exact path='/' component={Splash} />
-
-          <Route exact path='/drinks' render={()=><DrinksList drinksList={this.state.masterDrinkList}/>} />
+          <Route exact path='/'render={()=><DrinksList drinksList={this.state.masterDrinkList}/>}/>
 
           <Route exact path='/addDrink' render={()=><NewDrinkControl onNewDrink={this.handleAddingNewDrinkToList}/>} />
 
-          <Route path='/employees' render={(props)=><Employees drinksList={this.state.masterDrinkList}
-          currentRouterPath={props.location.pathname}
-          onDrinkSelection={this.state.handleEditingSelectedDrink}
-          selectedDrink={this.state.selectedDrink} />} />
+          <Route path='/employees' component={EmployeeConfirmation}/>
 
           <Route exact path='/about' component={About} />
           <Route component={Error404}/>
         </Switch>
-        <div>
-          <hr/>
-        </div>
       </div>
     );
   }
