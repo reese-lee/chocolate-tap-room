484000bc2b48b83f4f2c7cc8b050ef25fd550ee6
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 17:19:47 -0700

    edit props for Drink, remove keyId and replace index for key

--- src/components/DrinksList.jsx
+++ src/components/DrinksList.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import Drinks from './Drinks';
-import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
 function DrinksList(props) {
   return (
     <div>
-      {Object.keys(props.drinksList).map(function(drinkId){
-        let drink = props.drinksList[drinkId];
-        return <Drinks name={drinks.name}
-        price={drinks.price}
-        flavor={drinks.flavor}
-        pints={drinks.pints}
-        description={drinks.description}
-        key={drinkId}
-        drinkId={drinkId}
-        onDrinkSelection={props.onDrinkSelection}
-        />;
-      })}
+      {props.drinksList.map((drinks, index)=>
+        <Drinks
+          name={drinks.name}
+          price={drinks.price}
+          flavor={drinks.flavor}
+          description={drinks.description}
+          key={index}
+        />
+      )}
     </div>
   );
 }
 
 DrinksList.propTypes = {
-  drinksList: PropTypes.object,
-  currentRouterPath: PropTypes.string,
-  onDrinkSelection: PropTypes.func
+  drinksList: PropTypes.array
 };
 
 export default DrinksList;
