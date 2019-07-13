36119891331b50b15259a5bd3a0266b1e4e11b0f
Reese Lee <reese.lee.cy@gmail.com>
2019-07-12 17:19:14 -0700

    correct the properties for the drink object

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -2,40 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Drinks(props){
-  const drinkInformation =
-  <div>
-    <ul>
-      <li>{props.name}</li>
-      <li>{props.brand}</li>
-      <li>{props.price}</li>
-      <li>{props.description}</li>
-    </ul>
-  </div>;
-
-  if(props.currentRouterPath === '/employees') {
-    return (
-      <div onClick={()=> {props.onDrinkSelection(props.drinkId);}}>
-        {drinkInformation}
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        {drinkInformation}
-      </div>
-    );
-  }
+  return(
+    <div>
+      <ul>
+        <li>{props.name}</li>
+        <li>{props.price}</li>
+        <li>{props.flavor}</li>
+        <li>{props.description}</li>
+      </ul>
+    </div>
+  )
 }
 
 Drinks.propTypes = {
   name: PropTypes.string.isRequired,
-  brand: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  ticketid: PropTypes.string.isRequired,
-  currentRouterPath: PropTypes.string,
-  onDrinkSelection: PropTypes.func,
-  drinkId: PropTypes.string.isRequired
+  flavor: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
 };
 
 export default Drinks;
