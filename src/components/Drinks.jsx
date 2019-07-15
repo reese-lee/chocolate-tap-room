25cdb732be3fd4dfec43cd110720b3825dffce7d
Reese Lee <reese.lee.cy@gmail.com>
2019-07-14 18:41:52 -0700

    add Flavor and Description to display

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function Drinks(props){
-  const drinkInformation =
+  return (
   <div>
-    <ul>
-      <li>{props.name}</li>
-      <li>{props.price}</li>
-      <li>{props.flavor}</li>
-      <li>{props.description}</li>
-    </ul>
-  </div>;
-}
+      <h4>{props.name}</h4>
+      <p>{props.price}<br/>
+      Flavor: {props.flavor}<br/>
+      Description: {props.description}</p>
+  </div>
+)}
 
 Drinks.propTypes = {
   name: PropTypes.string.isRequired,
