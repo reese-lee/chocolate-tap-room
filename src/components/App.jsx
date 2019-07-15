fa6b82f5a49657a0d078148f8a042c244814020f
Reese Lee <reese.lee.cy@gmail.com>
2019-07-14 19:51:16 -0700

    remove import statement for makestyles:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import Error404 from './Error404';
 import DrinksList from './DrinksList';
 import Splash from './Splash';
