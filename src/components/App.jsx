deca17f3d304ad3426b49972ea2139078ec7edde
Reese Lee <reese.lee.cy@gmail.com>
2019-07-14 18:23:47 -0700

    add array of drinks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import NavBar from './NavBar';
 import AddDrink from './AddDrink';
 import Employees from './Employees';
 import About from './About';
-import { v4 } from 'uuid';
 
 class App extends React.Component {
   constructor(props){
@@ -19,6 +18,18 @@ class App extends React.Component {
         price: '$5.99',
         flavor: 'Spicy and Sweet',
         description: 'OOur world-famous old-fashioned milk chocolate with a kick of jalapeno and chili spices.'
+      },
+      {
+        name: 'The Old-Fashioned',
+        price: '$4.99',
+        flavor: 'Milk Chocolate',
+        description: "The original recipe that started in Grandma's kitchen, and built us into a nationwide chocolate force."
+      },
+      {
+        name: 'The Herb Garden',
+        price: '$6.99',
+        flavor: 'Earthy and herby',
+        description: 'Made with thyme, rosemary, and oregano, this one is straight from the backyard!'
       }],
       selectedDrink: null
     }
@@ -27,10 +38,7 @@ class App extends React.Component {
   }
 
   handleAddingNewDrinkToList(newDrink) {
-    let newDrinkId = v4()
-    let newMasterDrinkList = Object.assign({}, this.state.masterDrinkList, {
-      [newDrinkId]: newDrink
-    });
+    let newMasterDrinkList = this.state.masterDrinkList.push(newDrink);
     this.setState({masterDrinkList: newMasterDrinkList})
   }
 
@@ -49,10 +57,7 @@ class App extends React.Component {
 
           <Route exact path='/addDrink' render={()=><NewDrinkControl onNewDrink={this.handleAddingNewDrinkToList}/>} />
 
-          <Route path='/employees' render={(props)=><Employees drinksList={this.state.masterDrinkList}
-          currentRouterPath={props.location.pathname}
-          onDrinkSelection={this.state.handleEditingSelectedDrink}
-          selectedDrink={this.state.selectedDrink} />} />
+          <Route path='/employees' render={(props)=><Employees drinksList={this.state.masterDrinkList} />} />
 
           <Route exact path='/about' component={About} />
           <Route component={Error404}/>
